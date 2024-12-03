ffe037b1f462308e12ca7bab8d49ceccbd83c0dc
John Tromp <john.tromp@gmail.com>
2024-12-03 12:25:57 +0100

    day 3

---
+++ day3.hs
@@ -0,0 +1,24 @@
+import Data.List
+
+getmul :: String -> Int
+getmul ('m':'u':'l':s) = case reads s of
+  [((x,y),_)]   -> x * y
+  otherwise -> 0
+getmul _ = 0
+
+part1 :: String -> Int
+part1 = sum . map getmul . tails
+
+go :: Int -> Bool -> String -> Int
+go m _ [] = m
+go m _ ('d':'o':'(':')':s) = go m True s
+go m _ ('d':'o':'n':'\'':'t':'(':')':s) = go m False s
+go m True ('m':'u':'l':s) = go m True s + case reads s of
+  [((x,y),_)]   -> x * y
+  otherwise -> 0
+go m b (_:s) = go m b s
+
+part2 :: String -> Int
+part2 = go 0 True
+
+main = getContents >>= print . part2
